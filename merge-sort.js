456d68f91c67e3d5f2cd86925376f561746d3ba9
Brian McMillan <73612100+brian-mcmillan@users.noreply.github.com>
2023-03-28 20:41:02 -0400

    Create merge-sort.js

---
+++ merge-sort.js
@@ -0,0 +1,43 @@
+function merge(arr1, arr2){
+    // list containing merged items
+    let merged = [];
+    
+    // pointers
+    let i = 0;
+    let j = 0;
+    
+    // push the smaller value into merged
+    while(i < arr1.length && j < arr2.length){
+        if(arr2[j] > arr1[i]){
+            merged.push(arr1[i]))
+            i++;
+        } else {
+            merged.push(arr2[j])
+            j++;
+        }
+    }
+    // add leftover values into merged
+    while(i < arr1.length) {
+        merged.push(arr1[i])
+        i++;
+    }
+    while(j < arr2.length) {
+        merged.push(arr2[j])
+        j++;
+    }
+    return merged
+}
+
+
+function mergeSort(arr){
+    // base case 
+    if(arr.length <= 1) return arr;
+    
+    // halfway point of given arr
+    let mid = Math.floor(arr.length/2);
+    // left side arr
+    let lhs = mergeSort(arr.slice(0, mid));
+    // right side arr
+    let rhs = mergeSort(arr.slice(mid));
+    
+    return merge(lhs, rhs);
