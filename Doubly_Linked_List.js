06b09d5c1ee83d4d2009917e06cdd2d147a89c70
Brian McMillan <73612100+brian-mcmillan@users.noreply.github.com>
2023-04-21 14:47:31 -0400

    Update Doubly_Linked_List.js

--- Doubly_Linked_List.js
+++ Doubly_Linked_List.js
@@ -119,7 +119,7 @@ class DoublyLinkedList{
         }
     }
     remove(index){
-         if(index < 0 || index > this.length) return undefined;
+        if(index < 0 || index > this.length) return undefined;
         if(index === 0) return !!this.shift();
         if(index === this.length - 1) return !!this.pop();
         
