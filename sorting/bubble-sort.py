bc19415c67f1c3410257870261678beddb955643
Brian McMillan <73612100+brian-mcmillan@users.noreply.github.com>
2023-04-26 13:48:21 -0400

    Update bubble-sort.py

--- sorting/bubble-sort.py
+++ sorting/bubble-sort.py
@@ -4,7 +4,6 @@ def bubble_sort(arr):
     
     for i in range(size):
     
-        # default swapped to False.
         swapped = False
         
         # iterate through every arr index - i.
@@ -12,17 +11,12 @@ def bubble_sort(arr):
         for j in range(size-i):
             print(f"{arr}, COMPARING: {arr[j], arr[j+1]}")
             
-            # if the current element is greater than the next element
-            # swap the elements
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
                 swapped = True
                 
         print("PASS COMPLETE\n")
 
-        # if an entire pass through (size - i) is made
-        # and swapped is still False
-        # the array is already sorted, return.
         if not swapped:
             return arr
         
